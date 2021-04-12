2322f83b98798d780c628ea88e0404739036da20
dabljues <dabljues96@gmail.com>
2021-04-13 01:10:03 +0200

    Switch from JSS to SCSS. Add Remember me checkbox to the login page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -2,88 +2,75 @@ import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 import LockIcon from "@material-ui/icons/Lock";
-
-import "./LoginPage.css";
 import { Typography } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  box: {
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    translate: "translate(-50%, -50%)",
-  },
-  paper: {
-    padding: theme.spacing(5),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-  icon: {
-    margin: theme.spacing(1),
-  },
-  textfield: {
-    margin: theme.spacing(1),
-    width: "100%",
-  },
-  submit: {
-    marginTop: theme.spacing(2),
-    width: "100%",
-  },
-  grid: {},
-}));
+import "./LoginPage.scss";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const classes = useStyles();
+  const [rememberMe, setRememberMe] = useState(false);
 
   return (
-    <Container className={classes.box} component="main" maxWidth="xs">
-      <Paper className={classes.paper} variant="elevation">
-        <Avatar>
-          <LockIcon className={classes.icon} />
-        </Avatar>
-        <Typography variant="h6">Sign in</Typography>
+    <Box className="center" maxWidth="xs">
+      <Paper className="paper" variant="elevation">
+        <Box className="sign-in-logo">
+          <Avatar className="avatar">
+            <LockIcon />
+          </Avatar>
+          <Typography variant="h6">Sign in</Typography>
+        </Box>
         <TextField
+          className="textfield"
           label="Email"
           name="email"
           size="small"
           variant="outlined"
-          className={classes.textfield}
           onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
             setEmail(event.target.value);
           }}
         />
         <TextField
+          className="textfield"
           label="Password"
           name="password"
           size="small"
           variant="outlined"
           type="password"
-          className={classes.textfield}
           onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
             setPassword(event.target.value);
           }}
         />
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          onClick={() => {
-            console.log(email, password);
-          }}
-        >
-          Sign in
-        </Button>
+        <Box className="submit-form">
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={rememberMe}
+                onChange={() => setRememberMe(!rememberMe)}
+                color="primary"
+                name="rememberMe"
+              />
+            }
+            label="Remember me"
+          />
+          <Button
+            className="button"
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              console.log(email, password);
+            }}
+          >
+            Sign in
+          </Button>
+        </Box>
       </Paper>
-    </Container>
+    </Box>
   );
 };
 
