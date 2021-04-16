3939e7e6f2b29abec7c1d750a9dd21c5f3be2844
dabljues <dabljues96@gmail.com>
2021-04-17 01:11:17 +0200

    Use flex instead of grid in page header

--- src/PageHeader.tsx
+++ src/PageHeader.tsx
@@ -1,10 +1,7 @@
 import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import { Link, useHistory } from "react-router-dom";
 
@@ -21,36 +18,30 @@ const PageHeader = (props: HeaderProps) => {
   return (
     <AppBar>
       <Toolbar>
-        <Grid container>
-          <Grid container item xs={4}>
-            <Link to="/" className="grow" style={{ textDecoration: "none" }}>
-              <Avatar>PM</Avatar>
-            </Link>
-          </Grid>
-          <Grid container item className="title" xs={4}>
-            <Typography variant="h5">Project Manager</Typography>
-          </Grid>
-          <Grid container item className="account-buttons" xs={4}>
-            {!loggedIn ? (
-              <>
-                <Button
-                  className="header-button"
-                  variant="outlined"
-                  onClick={() => history.push("/login")}
-                >
-                  Login
-                </Button>
-                <Button
-                  className="header-button"
-                  variant="outlined"
-                  onClick={() => history.push("/register")}
-                >
-                  Register
-                </Button>
-              </>
-            ) : null}
-          </Grid>
-        </Grid>
+        <Link to="/" className="site-logo" style={{ textDecoration: "none" }}>
+          <Avatar>PM</Avatar>
+        </Link>
+        <Typography noWrap variant="h5" className="title">
+          Project Manager
+        </Typography>
+        {!loggedIn ? (
+          <div className="account-buttons">
+            <Button
+              className="header-button"
+              variant="outlined"
+              onClick={() => history.push("/login")}
+            >
+              Login
+            </Button>
+            <Button
+              className="header-button"
+              variant="outlined"
+              onClick={() => history.push("/register")}
+            >
+              Register
+            </Button>
+          </div>
+        ) : null}
       </Toolbar>
     </AppBar>
   );
