e8a6d4d356bbf13f69294d3dfbfb14f12d126e4d
dabljues <dabljues96@gmail.com>
2021-05-05 14:13:17 +0200

    Add current user endpoint

--- backend/users/api/views/user.py
+++ backend/users/api/views/user.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from users.api.serializers import UserSerializer
 from users.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=["get"])
+    def current(self, request, *args, **kwargs):
+        return Response(self.get_serializer(request.user).data)
