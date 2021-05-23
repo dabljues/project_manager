447df0bb68421441fb9d58c3ae8924bd9faff29c
dabljues <dabljues96@gmail.com>
2021-05-24 01:13:58 +0200

    Add project field to Task

--- backend/tasks/models/task.py
+++ backend/tasks/models/task.py
@@ -2,7 +2,9 @@ from django.db import models
 
 from .task_base import TaskBase
 from .utils import TASK_TYPES
+from projects.models import Project
 
 
 class Task(TaskBase):
     type = models.CharField("Type", max_length=1, default="T", choices=TASK_TYPES)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
