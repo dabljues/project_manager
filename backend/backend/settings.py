027b11659e17b4faeeb9a1f18ef586c623594f68
dabljues <dabljues96@gmail.com>
2021-05-24 02:42:07 +0200

    Change and hide Django secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "^b8^_dq#wn$#ywjc7l%x#q2vjl@&i)!4z)9=um($=+(ml3huf-"
+SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
