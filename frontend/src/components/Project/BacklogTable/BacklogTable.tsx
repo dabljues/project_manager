25d7b6437b2695c16cfd7f7f30e3bb703d29d399
dabljues <dabljues96@gmail.com>
2021-06-03 00:31:13 +0200

    Create BackLogTable

---
+++ frontend/src/components/Project/BacklogTable/BacklogTable.tsx
@@ -0,0 +1,36 @@
+import { TableColumnInterface, TableRowInterface } from "../../../types";
+import PaginatedTable from "../../PaginatedTable";
+
+interface BacklogTableProps {
+  rows: TableRowInterface[];
+}
+
+const BacklogTable = (props: BacklogTableProps) => {
+  const { rows } = props;
+  const columns: TableColumnInterface[] = [
+    {
+      id: "name",
+      label: "Name",
+      minWidth: 170,
+    },
+    { id: "status", label: "Status", minWidth: 50 },
+    { id: "title", label: "Title", minWidth: 50 },
+    { id: "type", label: "Type", minWidth: 50 },
+    {
+      id: "creator",
+      label: "Creator",
+      minWidth: 250,
+      align: "right",
+    },
+    {
+      id: "assignee",
+      label: "Assignee",
+      minWidth: 170,
+      align: "right",
+    },
+  ];
+
+  return <PaginatedTable columns={columns} rows={rows} rowKey="name" />;
+};
+
+export default BacklogTable;
--- frontend/src/containers/Project/Backlog/Backlog.tsx
+++ frontend/src/containers/Project/Backlog/Backlog.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 
 import { authRequest } from "../../../api/auth";
-import { TaskData } from "../../../types";
+import BacklogTable from "../../../components/Project/BacklogTable";
+import { TableRowInterface, TaskData } from "../../../types";
 
 interface BacklogParams {
   projectName: string;
@@ -13,24 +14,50 @@ interface BacklogParams {
 interface BacklogProps extends RouteComponentProps<BacklogParams> {}
 
 const Backlog = ({ match }: BacklogProps) => {
-  const [tasks, setTasks] = useState<TaskData[]>([]);
+  const [rows, setRows] = useState<TableRowInterface[]>([]);
   const { projectName } = match.params;
 
   const authCommunicator = authRequest();
   useEffect(() => {
     const getTasks = async () => {
-      const projectTasks = await authCommunicator
-        .get(`/project/${projectName}/backlog`)
-        .then((response) => response.data);
-      setTasks(projectTasks);
+      const tasksCollected: TableRowInterface[] = [];
+      const projectTasks = await authCommunicator.get(
+        `/project/${projectName}/backlog`
+      );
+      await Promise.all(
+        projectTasks.data.map(async (taskData: TaskData) => {
+          const { creator, assignee } = taskData;
+          const creatorString =
+            creator === null
+              ? "<unassigned>"
+              : `${creator.firstName} ${creator.lastName}`;
+          const assigneeString =
+            assignee === null
+              ? "<unassigned>"
+              : `${assignee.firstName} ${assignee.lastName}`;
+          tasksCollected.push({
+            name: taskData.name,
+            status: taskData.status,
+            title: taskData.title,
+            type: taskData.type,
+            creator: creatorString,
+            assignee: assigneeString,
+          });
+        })
+      );
+      setRows(tasksCollected);
     };
     getTasks();
     return () => {
-      setTasks([]);
+      setRows([]);
     };
   }, []);
 
-  return <div>{projectName}</div>;
+  return (
+    <div className="center">
+      <BacklogTable rows={rows} />{" "}
+    </div>
+  );
 };
 
 export default withRouter(Backlog);
