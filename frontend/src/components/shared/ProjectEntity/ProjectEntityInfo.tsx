055c067874c25f06fb801a7b786196ce560c444b
dabljues <dabljues96@gmail.com>
2021-06-10 01:41:35 +0200

    Use styled-components in Project page

---
+++ frontend/src/components/shared/ProjectEntity/ProjectEntityInfo.tsx
@@ -0,0 +1,8 @@
+import { css } from "styled-components";
+
+const ProjectEntityInfo = css`
+  margin-top: 20px;
+  padding: 20px;
+`;
+
+export default ProjectEntityInfo;
---
+++ frontend/src/components/shared/ProjectEntity/index.js
@@ -0,0 +1,3 @@
+export { default as ProjectEntity } from "./ProjectEntity";
+export { default as ProjectEntityInfo } from "./ProjectEntityInfo";
+export { default as ProjectEntityName } from "./ProjectEntityName";
