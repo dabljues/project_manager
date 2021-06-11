b22d75c6b7d601c9ac07952051e4c3eef8bb15d8
dabljues <dabljues96@gmail.com>
2021-06-11 04:29:11 +0200

    Use styled-components in CreateProjectForm

---
+++ frontend/src/components/Project/CreateProjectForm/CreateProjectForm.styles.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components/macro";
+
+import { Avatar, Button, Paper, TextField } from "@material-ui/core";
+import GroupWorkIcon from "@material-ui/icons/GroupWork";
+
+const CreateProjectForm = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+  min-width: 400px;
+  border: var(--paper-border);
+`;
+
+const ProjectIcon = styled(GroupWorkIcon)`
+  color: var(--project-color);
+  font-size: 3rem;
+`;
+
+const CreateProjectLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0px;
+  margin-bottom: 3.5rem;
+  text-align: center;
+
+  p {
+    font-size: 1.5rem;
+  }
+`;
+
+const FormButton = styled(Button)`
+  margin-top: 20px;
+  width: 100%;
+`;
+
+const FormTextField = styled(TextField)`
+  margin: 10px;
+  width: 100%;
+`;
+
+const FormAvatar = styled(Avatar)`
+  margin-bottom: 8px;
+`;
+
+export {
+  CreateProjectForm,
+  CreateProjectLogo,
+  FormAvatar,
+  FormButton,
+  FormTextField,
+  ProjectIcon,
+};
---
+++ frontend/src/components/shared/FormErrors/FormErrors.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components/macro";
+
+const FormErrors = styled.div`
+  background-color: rgb(255, 210, 218);
+  border-radius: 12px;
+  padding: 10px;
+  margin-bottom: 20px;
+  text-align: center;
+
+  li {
+    text-transform: lowercase;
+  }
+`;
+
+export default FormErrors;
