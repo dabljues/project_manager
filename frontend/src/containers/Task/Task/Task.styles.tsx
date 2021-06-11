8cda5bbd802adca3bcf3e741cb8c192e7fab6307
dabljues <dabljues96@gmail.com>
2021-06-11 04:59:49 +0200

    Remove all conventional styling from Task page

--- frontend/src/containers/Task/Task/Task.styles.tsx
+++ frontend/src/containers/Task/Task/Task.styles.tsx
@@ -1,19 +1,21 @@
-import styled from "styled-components";
 import {
   ProjectEntity,
+  ProjectEntityHeader,
+  ProjectEntityIcon,
   ProjectEntityInfo,
   ProjectEntityName,
 } from "components/shared/ProjectEntity";
+import styled from "styled-components";
 
-const Task = styled.div`
-  ${ProjectEntity}
-`;
+import AssignmentIcon from "@material-ui/icons/Assignment";
 
-const TaskInfo = styled.div`
-  ${ProjectEntityInfo}
+const Task = styled(ProjectEntity)``;
+const TaskHeader = styled(ProjectEntityHeader)``;
+const TaskIcon = styled(AssignmentIcon)`
+  ${ProjectEntityIcon}
+  color: white;
 `;
+const TaskName = styled(ProjectEntityName)``;
+const TaskInfo = styled(ProjectEntityInfo)``;
 
-const TaskName = styled.div`
-  ${ProjectEntityName}
-`;
-export { Task, TaskInfo, TaskName };
+export { Task, TaskHeader, TaskIcon, TaskInfo, TaskName };
