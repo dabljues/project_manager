44380c2b688939104db24e431e93ecd5b6c14251
dabljues <dabljues96@gmail.com>
2021-06-12 19:05:05 +0200

    Move ProjectEntity styles to one file and use styled
    The problem with importing just one styled.<something> persists - no hot
    reload. But now there is a css import which will trigger the hot reload.

--- frontend/src/components/shared/ProjectEntity/ProjectEntityIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { css } from "styled-components/macro";
-
-const ProjectEntityIcon = css`
-  font-size: 55px;
-  margin-right: 15px;
-`;
-
-export default ProjectEntityIcon;
--- frontend/src/containers/Task/Task/Task.styles.tsx
+++ frontend/src/containers/Task/Task/Task.styles.tsx
@@ -9,21 +9,13 @@ import styled from "styled-components";
 
 import AssignmentIcon from "@material-ui/icons/Assignment";
 
-const Task = styled.div`
-  ${ProjectEntity}
-`;
-const TaskHeader = styled.div`
-  ${ProjectEntityHeader}
-`;
+const Task = styled(ProjectEntity)``;
+const TaskHeader = styled(ProjectEntityHeader)``;
 const TaskIcon = styled(AssignmentIcon)`
   ${ProjectEntityIcon}
   color: white;
 `;
-const TaskName = styled.span`
-  ${ProjectEntityName}
-`;
-const TaskInfo = styled.div`
-  ${ProjectEntityInfo}
-`;
+const TaskName = styled(ProjectEntityName)``;
+const TaskInfo = styled(ProjectEntityInfo)``;
 
 export { Task, TaskHeader, TaskIcon, TaskInfo, TaskName };
