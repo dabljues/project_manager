137f1d925ed9a8619769d291bcc313cb828ee3ee
dabljues <dabljues96@gmail.com>
2021-06-14 03:44:49 +0200

    Add 404 page

---
+++ frontend/src/containers/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import CenteredDiv from "components/shared/CenteredDiv";
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+const NotFoundPage = () => (
+  <CenteredDiv>
+    <Title level={1}>Page not Found</Title>
+  </CenteredDiv>
+);
+
+export default NotFoundPage;
