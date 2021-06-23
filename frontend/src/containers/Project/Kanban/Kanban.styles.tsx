0b3213cd5b91ba23fae8492133d59d992065ea37
dabljues <dabljues96@gmail.com>
2021-06-23 11:55:16 +0200

    Add Kanban board page

---
+++ frontend/src/containers/Project/Kanban/Kanban.styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components/macro";
+
+const Kanban = styled.div``;
+const x = styled.div``;
+
+export { Kanban, x };
---
+++ frontend/src/containers/Project/Kanban/Kanban.tsx
@@ -0,0 +1,8 @@
+import * as S from "./Kanban.styles";
+
+const Kanban = () => {
+  const x = 2 + 2;
+  return <div>{x}</div>;
+};
+
+export default Kanban;
