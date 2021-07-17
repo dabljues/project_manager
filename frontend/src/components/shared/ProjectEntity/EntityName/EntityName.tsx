9dcda170bd1d8d20423471963b930d2a929ae661
dabljues <dabljues96@gmail.com>
2021-07-17 02:16:26 +0200

    Make ProjectEntity Header more responsive

--- frontend/src/components/shared/ProjectEntity/EntityName/EntityName.tsx
+++ frontend/src/components/shared/ProjectEntity/EntityName/EntityName.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components/macro";
 
-import { Avatar, CardHeader, Typography } from "@material-ui/core";
+import {
+  CardHeader,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 
 const Icon = styled(AssignmentIcon)`
@@ -8,9 +13,21 @@ const Icon = styled(AssignmentIcon)`
   font-size: 4rem;
 `;
 
+const NameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Name = styled(Typography)`
   color: white;
-  font-size: 4rem;
+  font-size: 1rem;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.values.xs}px) {
+    font-size: 2rem;
+  }
+  @media (min-width: ${(props) => props.theme.breakpoints.values.sm}px) {
+    font-size: 4rem;
+  }
 `;
 
 const StyledHeader = styled(CardHeader)`
@@ -19,8 +36,28 @@ const StyledHeader = styled(CardHeader)`
   align-items: center;
 `;
 
-const EntityName = ({ name }: { name: string }) => (
-  <StyledHeader avatar={<Icon />} title={<Name>{name}</Name>} />
-);
+const EntityName = ({ name }: { name: string }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  if (matches) {
+    return (
+      <StyledHeader
+        disableTypography
+        avatar={<Icon />}
+        title={<Name>{name}</Name>}
+      />
+    );
+  }
+  return (
+    <StyledHeader
+      disableTypography
+      title={
+        <NameWrapper>
+          <Name>{name}</Name>
+        </NameWrapper>
+      }
+    />
+  );
+};
 
 export default EntityName;
